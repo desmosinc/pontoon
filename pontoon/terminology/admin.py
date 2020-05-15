8b3bf90a1c3c33179b76249185f95c3dccdc4b9d
Anand Thakker <github@anandthakker.net>
2020-05-15 08:42:15 -0400

    Show locale code in term translation admin form

--- pontoon/terminology/admin.py
+++ pontoon/terminology/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django import forms
 
 from pontoon.terminology.models import Term, TermTranslation
+from pontoon.base.models import Locale
 
 
 class TermAdmin(admin.ModelAdmin):
@@ -33,15 +35,24 @@ class TermAdmin(admin.ModelAdmin):
     )
 
 
+class LocaleChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "{} ({})".format(obj.name, obj.code)
+
+
 class TermTranslationAdmin(admin.ModelAdmin):
     search_fields = [
         "text",
     ]
-    list_display = (
-        "text",
-        "term",
-        "locale",
-    )
+    list_display = ("text", "term", "locale", "locale_code")
+
+    def locale_code(self, term_translation):
+        return term_translation.locale.code
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "locale":
+            return LocaleChoiceField(queryset=Locale.objects.all())
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(Term, TermAdmin)
